6f5c90d5750445056d1d9a35588e8f39bdf7a692
Pat <pmclean1319@gmail.com>
2017-09-09 21:19:15 -0400

    PlayerBehavior: Pushes the player's avatar forward indefinitely according to a set rate. NewTileTrigger: When the player's avatar enters a midpoint in a tile, as represented as a trigger collider, a new tile is instantiated ahead of the current one.

---
+++ NewTileTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewTileTrigger : MonoBehaviour {
+
+    public GameObject newTile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void OnTriggerEnter(Collider player)
+    {
+        spawnNewTile();
+    }
+
+    private void spawnNewTile()
+    {
+        Instantiate(newTile, new Vector3(transform.position.x, transform.position.y, transform.position.z + 49), Quaternion.identity);
+    }
+}
---
+++ PlayerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public float speed;
+    public float maxSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<Rigidbody>().velocity.z < maxSpeed)
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        }
+	}
+}
